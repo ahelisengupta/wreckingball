cfadf843e3f9f78f99c021a42d52295199424cb7
ahelisengupta <70891431+ahelisengupta@users.noreply.github.com>
2020-11-20 18:10:13 +0530

    Add files via upload

---
+++ Ball.js
@@ -0,0 +1,23 @@
+class Ball{
+    constructor(x,y,r){
+        var options = {
+            frictionAir:0.005,
+            density:1
+        }
+        this.body=Bodies.circle(x,y,r,options)
+        this.r = r;
+        World.add(world,this.body);
+    }
+
+    display(){
+        var pos = this.body.position;
+        var angle = this.body.angle;
+        push();
+        translate(pos.x,pos.y);
+        rotate(angle);
+        fill("red");
+        ellipseMode(CENTER);
+        ellipse(0,0,this.r);
+        pop();
+    }
+}
